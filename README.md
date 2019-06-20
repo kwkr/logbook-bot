b64fccd06a8c58615c60c8119543ed921b5b80a7
Maciej Kawka <maciej.kawka@objective-partner.com>
2019-06-20 13:57:18 +0200

    added parsing logminion export and added saving

--- README.md
+++ README.md
@@ -6,11 +6,11 @@ to run it:
 `npm i`
 `npm run fill`
 
-You can find source data format in `toFill.json` file. The bot will use it to fill weekly view (only current week) with the projects and corresponding task with the data from file. Objects in `hours:[...]` corresponds to subsequent days of the week (0 for monday, 1 for tuesday etc.). Each of them contains information about how much time to fill (`time:"2:00"`) and how to comment it (`comment:"I was doing nothing"`).
+You can find source data format in `export.json` file. The bot will use it to fill weekly view (only current week) with the projects and corresponding task with the data from file. Objects in `hours:[...]` corresponds to subsequent days of the week (0 for monday, 1 for tuesday etc.). Each of them contains information about how much time to fill (`time:"2:00"`) and how to comment it (`comment:"I was doing nothing"`).
 
 For tracking your time while at work you can use my other tool:
 
-[LogBook](http://logbook.com.s3-website.eu-central-1.amazonaws.com/)
+[LogMinion](logminion.com)
 
 It allows yout to seamlessly track your activities and then export it as json.
 Perfect input for this kind of bot!
--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 require('dotenv').config();
 const puppeteer = require('puppeteer');
-const delayObject = { delay: 10 };
+const delayObject = { delay: 20 };
 const tableSelector =
   'table[class=" list fixableTableHeader_jq ListDisplay sizable orderable"] > tbody';
 
 let toSave;
 try {
-  toSave = require('./toFill.json');
-} catch (e) {}
+  toSave = require('./export.json');
+  if (!Array.isArray(toSave)) {
+    console.log('Converting from Logbook.com format');
+    toSave = convertFromLogminionFormat(toSave);
+  }
+} catch (e) {
+  console.log(e);
+}
 if (!toSave || toSave.length === 0) {
   console.log('No data to book. Exiting...');
   process.exit();
@@ -31,7 +37,8 @@ async function run() {
   await page.waitFor(100);
   await page.waitForSelector('input[data-bcs-button-name="Apply"]');
   //uncomment to save
-  //await page.click('input[data-bcs-button-name="Apply"]');
+  await page.click('input[data-bcs-button-name="Apply"]');
+  await page.waitFor(2000);
   await browser.close();
 }
 
@@ -147,10 +154,34 @@ async function fillWeek(page, projectRowData) {
   await asyncForEach(projectRowData, async projectData => {
     let informationsToFill = getInformationToFillForProject(projectData.data);
     let projectRowIndex = projectData.index;
+    console.log(
+      'Processing project: ',
+      projectData.data[0] + ' ' + projectData.data[1]
+    );
     await asyncForEach(informationsToFill.hours, async (hour, dayIndex) => {
+      console.log('Processing day: ', getWeekDayString(dayIndex));
       await fillDay(page, hour, dayIndex, projectRowIndex, mondayTimeStamp);
     });
   });
+}
+
+function getWeekDayString(day) {
+  switch (day + 1) {
+    case 1:
+      return 'Mon';
+    case 2:
+      return 'Tue';
+    case 3:
+      return 'Wed';
+    case 4:
+      return 'Thu';
+    case 5:
+      return 'Fri';
+    case 6:
+      return 'Sat';
+    case 7:
+      return 'Sun';
+  }
 }
 
 async function fillDay(page, hour, dayIndex, projectRowIndex, mondayTimeStamp) {
@@ -160,6 +191,11 @@ async function fillDay(page, hour, dayIndex, projectRowIndex, mondayTimeStamp) {
   const hourCount = timeSplit[0];
   const minutesCount = timeSplit[1];
   if (isEmpty(comment) || isEmpty(hourCount) || isEmpty(minutesCount)) {
+    console.log('nothing to process');
+    return;
+  }
+  if (parseInt(hourCount, 10) === 0 && parseInt(minutesCount, 10) === 0) {
+    console.log('no time booked for that day');
     return;
   }
   await page.waitFor(1000);
@@ -172,11 +208,21 @@ async function fillDay(page, hour, dayIndex, projectRowIndex, mondayTimeStamp) {
 
 async function fillHours(page, projectRowIndex, dayIndex, hourCount) {
   let hoursInputKey = `0_${projectRowIndex}_${5 + dayIndex}_0`;
+  await page.waitFor(500);
+  await page.focus(`input[id="${hoursInputKey}"]`);
+  await page.keyboard.press('Backspace');
+  await page.type(`input[id="${hoursInputKey}"]`, hourCount, delayObject);
+  await page.keyboard.press('Backspace');
   await page.type(`input[id="${hoursInputKey}"]`, hourCount, delayObject);
 }
 
 async function fillMinutes(page, projectRowIndex, dayIndex, minutesCount) {
   let minutesInputKey = `0_${projectRowIndex}_${5 + dayIndex}_1`;
+  await page.waitFor(500);
+  await page.focus(`input[id="${minutesInputKey}"]`);
+  await page.keyboard.press('Backspace');
+  await page.type(`input[id="${minutesInputKey}"]`, minutesCount, delayObject);
+  await page.keyboard.press('Backspace');
   await page.type(`input[id="${minutesInputKey}"]`, minutesCount, delayObject);
 }
 
@@ -194,7 +240,7 @@ async function fillComment(page, comment) {
   await page.waitForSelector(textAreaSelector);
   await page.waitFor(100);
   await page.$eval(textAreaSelector, el => el.focus());
-  await page.keyboard.type(comment, { delay: 100 });
+  await page.keyboard.type(comment, { delay: 10 });
 }
 
 async function closeCommentDialog(page) {
@@ -207,6 +253,110 @@ async function closeCommentDialog(page) {
     delay: 100
   });
   await page.waitFor(500);
+}
+
+function convertFromLogminionFormat(file) {
+  let toBook = [];
+  Object.keys(file).forEach(dayKey => {
+    let processingDayDate = new Date(parseInt(dayKey, 10));
+    if (!isDateThisWeek(processingDayDate)) {
+      return;
+    }
+    let dayInfo = file[dayKey];
+    Object.keys(dayInfo).forEach(taskName => {
+      if (!taskName.includes('|')) {
+        return;
+      }
+      const taskParts = taskName.split('|');
+      let botProject = taskParts[0].trim();
+      let botTask = taskParts[1].trim();
+
+      toBook = addTaskIfNotInArray(toBook, botProject, botTask);
+      const logs = dayInfo[taskName];
+      const dayNumber = processingDayDate.getDay();
+      toBook = addLogsToBook(toBook, botProject, botTask, dayNumber, logs);
+    });
+  });
+
+  return toBook;
+}
+
+function isDateThisWeek(date) {
+  //SUNDAY is 0
+  const MS_IN_DAY = 86400 * 1000;
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+  const diff = today.getTime() - date.getTime();
+  const daysDiff = diff / MS_IN_DAY;
+  const maxDiff = today.getDay() - 1;
+  if (daysDiff <= maxDiff && daysDiff >= 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function addTaskIfNotInArray(toBook, projectName, taskName) {
+  let found = false;
+  toBook.forEach(task => {
+    if (found) {
+      return;
+    }
+    found = task.project === projectName && task.task === taskName;
+  });
+  if (!found) {
+    toBook.push({
+      project: projectName,
+      task: taskName,
+      hours: [
+        { comment: '', time: '0:00' },
+        { comment: '', time: '0:00' },
+        { comment: '', time: '0:00' },
+        { comment: '', time: '0:00' },
+        { comment: '', time: '0:00' }
+      ]
+    });
+  }
+  return toBook;
+}
+
+function addLogsToBook(toBook, projectName, taskName, day, logs) {
+  const arrayIndexDay = day - 1;
+  if (arrayIndexDay > 4 || arrayIndexDay < 0) {
+    console.log('task is not during week');
+    return;
+  }
+
+  toBook.forEach(task => {
+    if (task.project === projectName && task.task === taskName) {
+      let totalDuration = getLogsTotalDuration(logs);
+      let stringDuration =
+        String((totalDuration - (totalDuration % 60)) / 60) +
+        ':' +
+        String(totalDuration % 60);
+      let summary = getSummaryDescription(logs);
+      task.hours[arrayIndexDay].comment = summary;
+      task.hours[arrayIndexDay].time = stringDuration;
+    }
+  });
+  return toBook;
+}
+
+function getLogsTotalDuration(logs) {
+  let totalDurationInMinutes = 0;
+  logs.forEach(log => {
+    totalDurationInMinutes += Math.floor(log.duration / 60);
+  });
+
+  return totalDurationInMinutes;
+}
+
+function getSummaryDescription(logs) {
+  let summary = '';
+  logs.forEach(log => {
+    summary += log.description + ', ';
+  });
+  return summary;
 }
 
 run();
